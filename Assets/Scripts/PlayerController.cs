cb28fb60d623c275738ad3616cb65260fc5811fb
Valchuk Dmitry <dart.fk712@gmail.com>
2018-08-11 10:49:56 +0600

    add player control

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : IPlayerController
+{
+
+	public float Velocity = 1f;
+	
+	public void Move(Rigidbody2D mybody) {
+		Vector3 velocity = new Vector3(Input.GetAxis("Horizontal") * Velocity, Input.GetAxis("Vertical") * Velocity, 0);
+		Debug.Log(velocity);
+		mybody.transform.position = mybody.transform.position + velocity;
+	}
+}
