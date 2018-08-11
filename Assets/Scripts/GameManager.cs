fc2ddef2810f11647c92e8dee844551d5be76c5c
Valchuk Dmitry <dart.fk712@gmail.com>
2018-08-11 13:14:05 +0600

    add scenes for menu and game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DefaultNamespace;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 	public List<GameObject> players;
-
+	private IField _field;
 	void Start () {
 		for (int i = 0; i < players.Count; i++)
 		{
 			Instantiate(players[i]);
 		}
+		_field.GenerateMap();
+		_field.RenderMap();
 	}
 }
---
+++ Assets/Scripts/MenuButtonsHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButtonsHandler : MonoBehaviour {
+
+	public Button NewGameButton, ExitButton;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		NewGameButton.onClick.AddListener(NewGameButtonHandler);
+		ExitButton.onClick.AddListener(ExitButtonHandler);
+		
+	}
+
+	void NewGameButtonHandler()
+	{
+		Application.LoadLevel("GameScene");
+	}
+	
+	void ExitButtonHandler()
+	{
+		Application.Quit();
+	}
+}
