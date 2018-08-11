2aa7b74f0de3a34f70c36f6904da80745351f78a
Ivan Sharun <ivan@sha.run>
2018-08-11 18:41:16 +0600

    Interface and tile types for field

---
+++ Assets/Scripts/IField.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.Tilemaps;
+
+namespace DefaultNamespace
+{
+    public interface IField
+    {
+        void setTilemap(Tilemap tilemap);
+        void GenerateMap();
+        void RenderMap();
+        FieldTileType[,] getArray();    
+    }
+}
